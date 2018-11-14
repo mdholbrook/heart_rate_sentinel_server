c83d0cfd931b355c96b9411a4203546fcdf707ae
Matt Holbrook <mdh54@duke.edu>
2018-11-13 22:20:37 -0500

    Added unit files (didn't get added before)

---
+++ unit_tests/test_verify_inputs.py
@@ -0,0 +1,52 @@
+import pytest
+from functions.verify_inputs import *
+
+
+@pytest.mark.parametrize("candidate, expected", [
+    ('test', False),
+    ([1, 3, 4, 2], False),
+    ({'test': 'dict'}, True)])
+def test_is_dictionary(candidate, expected):
+
+    # Run the test
+    assert is_dictionary(candidate) == expected
+
+
+@pytest.mark.parametrize("required_key, dict, expected", [
+    ('places', {'people': 'mark', 'places': 'SLC', }, True),
+    ('places', {'people': 'mark', 'things': 'apples', }, False),
+    ('funds', {'funds': [10, 12, 5.1], 'things': 'apples', }, True)
+    ])
+def test_contains_key(required_key, dict, expected):
+
+    # Run the test
+    assert contains_key(required_key, dict) == expected
+
+
+@pytest.mark.parametrize("candidate, expected, expected_int", [
+    (5, True, 5),
+    (6.2, True, 6),
+    ('Summer', False, 'Summer'),
+    ('3', True, 3),
+    ('9.2', False, '9.2')
+    ])
+def test_is_numeric(candidate, expected, expected_int):
+
+    # Run the test
+    t, num = is_numeric(candidate)
+
+    assert t == expected
+    assert num == expected_int
+
+
+@pytest.mark.parametrize("candidate, expected", [
+    (12, False),
+    ('jim@jim.edu', True),
+    ('jim.jim.edu', False),
+    ("o'hare@gmail.com", True),
+    ('9.2', False)
+    ])
+def test_is_numeric(candidate, expected):
+
+    # Run the test
+    assert valid_email(candidate) == expected
