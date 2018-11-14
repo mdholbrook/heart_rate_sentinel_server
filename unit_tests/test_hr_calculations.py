cb4a7b17f55668ef3f091ed0f0e7c7fd6061d0a6
Matt <mdh54@duke.edu>
2018-11-14 14:36:03 -0500

    Corrected unit tests for pep8

--- unit_tests/test_hr_calculations.py
+++ unit_tests/test_hr_calculations.py
@@ -3,10 +3,10 @@ from functions.hr_calculations import *
 
 
 @pytest.mark.parametrize("candidate, database, expected", [
-    ('jack', [{'patient_ids': 'jump'}, {'patient_id': 'jack'}], 1),
-    ('jungle', [{'patient_ids': 'jungle'}, {'patient_id': 'jack'}], 0),
-    ('bo', [{'patient_ids': 'james'}, {'patient_id': 'boo'},
-            {'patient_ids': 'bo'}], 2)])
+    ('jack', [{'patient_id': 'jump'}, {'patient_id': 'jack'}], 1),
+    ('jungle', [{'patient_id': 'jungle'}, {'patient_id': 'jack'}], 0),
+    ('bo', [{'patient_id': 'james'}, {'patient_id': 'boo'},
+            {'patient_id': 'bo'}], 2)])
 def test_find_id_ind(candidate, database, expected):
 
     # Run the test
@@ -14,12 +14,14 @@ def test_find_id_ind(candidate, database, expected):
 
 
 @pytest.mark.parametrize("candidate, database, expected", [
-    ('jump', [{'patient_ids': 'jump', 'heart_rate': [50, 60, 70]},
-              {'patient_ids': 'jack', 'heart_rate': [120, 112, 131]}], [50, 60, 70]),
-    ('jack', [{'patient_ids': 'jump', 'heart_rate': [50, 60, 70]},
-              {'patient_ids': 'jack', 'heart_rate': [120, 112, 131]}], [120, 112, 131])
+    ('jump', [{'patient_id': 'jump', 'heart_rate': [50, 60, 70]},
+              {'patient_id': 'jack', 'heart_rate': [120, 112, 131]}],
+     [50, 60, 70]),
+    ('jack', [{'patient_id': 'jump', 'heart_rate': [50, 60, 70]},
+              {'patient_id': 'jack', 'heart_rate': [120, 112, 131]}],
+     [120, 112, 131])
      ])
 def test_get_heart_rates(candidate, database, expected):
 
     # Run the test
-    assert all(get_heart_rates(candidate, database) == expected)
+    assert get_heart_rates(candidate, database) == expected
